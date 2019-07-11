4c71a3dfbf75b1245deb31001f7b917cfb102718
Your Name <email@example.com>
2019-07-11 11:26:41 +0800

    swiperTest

---
+++ ppt/menu.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$(function(){
+$("#menuc_right").click(function(){
+$("#menuc").animate({right: "-0px"}, 1000);
+$("#menuc_right").hide();
+$("#menuc_righ").show();
+})
+$("#menuc_righ").click(function(){
+$("#menuc").animate({right: "-111px"}, 1000);
+$("#menuc_righ").hide();
+$("#menuc_right").show();
+})
+})
